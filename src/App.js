fbd93028aac438b06b03825e43b3c247cfcb103a
lindsayellis22 <lindsayellis39@gmail.com>
2022-02-14 13:39:39 -0500

    render api on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 // import Radclyf from '/Users/lindsayellis/SEI/projects/project4-front/project4-front/src/components/Radclyf.js';
-import Nav from './components/Home.js';
+import Nav from './components/Nav.js';
 import Home from '/Users/lindsayellis/SEI/projects/project4-front/project4-front/src/components/Home.js';
 
 function App() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
-import Select from 'react-select';
 import { useState, useEffect } from 'react';
 // import Radclyf from './Radclyf';
 
-
-const Home = props => {   
-    const [pets, setPets] = useState([]);
-    const [error, setError] = useState('[]');
+function Home(props) {   
+    const [species, setSpecies] = useState([]);
     const [loading, setLoading] = useState(true);
+    
 
     useEffect(() => {
-    const url = `http://localhost:3111/pets`;
-    setLoading(true);
-    fetch(url)
-        .then((res) => res.json())
-        .then((res) => {
-            setPets(res.results);
-            console.log(res)
-            setLoading(false);
-        },
-            (error) => {
+        
+        fetch('http://localhost:3111/species')
+            .then((res) => res.json())
+            .then((res) => {
+                setSpecies(res);
                 setLoading(false);
-                setError(error);
-              }
-            )
-        }, [])
+            }).catch((err) => console.error(`Oops, something went wrong: ${err}`));
+        }, []);
       
-        if (error) {
-            return <div>Error: zomg!</div>;
-          } else if (loading) {
-            return <div>Loading...</div>;
-          }
+        if (loading) {return <h1>Loading...</h1>};
 
     return (
         <div>
             <TestHeading>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quam dolorum corrupti consectetur sunt temporibus suscipit doloribus debitis, fugit nobis ipsa, omnis quas qui dignissimos dolorem! Voluptate possimus consectetur officiis!</TestHeading>
             <ul>
-                {pets.map(pet => (
-                    <li key={pet.id}>
-                        {pet.name} {pet.color}
+                {species.map(speciespets => (
+                    <li key={speciespets.id}>
+                        {speciespets.speciesName} {speciespets.defaultColor}
                     </li>
                 ))}
             </ul>
         </div>
     );
   }
+
+
 
             
 
