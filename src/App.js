44a98c3b7e840a254aebf13cd00fd6f6d96f6988
lindsayellis22 <lindsayellis39@gmail.com>
2022-02-18 02:38:01 -0500

    changed app.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Nav from './components/Nav.js';
 import Home from '/Users/lindsayellis/SEI/projects/project4-frontend/src/components/Home.js';
 import CreatePet from '/Users/lindsayellis/SEI/projects/project4-frontend/src/components/CreatePet.js'
 import AdoptedPets from '/Users/lindsayellis/SEI/projects/project4-frontend/src/components/AdoptedPets.js'
-import GlobalStyles from '/Users/lindsayellis/SEI/projects/project4-frontend/src/globalStyles.js';
 import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <GlobalStyles />
         <Nav />
       </header>
       <Layout>
