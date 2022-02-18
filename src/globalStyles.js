017f9ee0abe8e2b689436bfc7c611e031976f1f1
lindsayellis22 <lindsayellis39@gmail.com>
2022-02-18 02:33:43 -0500

    removed styles

--- src/globalStyles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-    body {
-        background: #b8b8ff;
-        font-family: 'Bakbak One', sans-serif;
-    }
-`
-export default GlobalStyle;
